f4670fe0aab02d4f16ecfef115bca3e2d19719cb
Rayden <88407660+cekwr@users.noreply.github.com>
2021-08-04 15:13:16 +0700

    Update global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -3,14 +3,16 @@ function welcomeMsg() {
     let array = 0;
 
     
-    const wAgw = `<a href="https://api.whatsapp.com/send?phone=6282177344150" target="_blank">Rayden</a>`;
+    const wAgw = `<a href="https://api.whatsapp.com/send?phone=6282177344150" target="_blank">WhatsApp</a>`;
     
     
-    const msg3 = `<p class="animation mb-0 text-truncate">Ready JOKI WR - Hide history pertandingan - Create akun advance server</p>`;
-    const msg4 = `<p class="animation mb-0 text-truncate">Chat WA ${wAgw}</p>`;
-    const msg5 = `<p class="animation mb-0 text-truncate">Selamat menggunakan tools ini ;)</p>`;
+    const msg1 = `<p class="animation mb-0 text-truncate">Ready JOKI WR</p>`;
+    const msg4 = `<p class="animation mb-0 text-truncate">Hide history pertandingan`;
+    const msg5 = `<p class="animation mb-0 text-truncate">Create akun advance server</p>`;
+    const msg2 = `<p class="animation mb-0 text-truncate">Minat Chat ${wAgw}</p>`;
+    const msg3 = `<p class="animation mb-0 text-truncate">Selamat menggunakan tools ini ;)</p>`;
 
-    let arrayEl = [msg3, msg4, msg5];
+    let arrayEl = [msg1, msg4, msg5, msg2, msg3];
     let arrayMax = 5;
     setInterval(() => {
         welcomeMsgEl.innerHTML = arrayEl[array];
